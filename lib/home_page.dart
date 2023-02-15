49bfaa87769e1c7c54570e6293eb61e6e6f7b1f9
aungkaungmyat <aungkaungmyat.mce@gmail.com>
2023-02-15 21:49:17 +0630

    Lesson5

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_sample/controller/home_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +11,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GetStorage storage = GetStorage();
+  TextEditingController emailController = TextEditingController();
+  HomeController homeController = HomeController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetStorage'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  if (GetUtils.isEmail(emailController.text)) {
+                    storage.write('email', emailController.text);
+                    emailController.text = '';
+                  } else {
+                    Get.snackbar(
+                      'Incorrect Email',
+                      'Enter a valid email',
+                      snackPosition: SnackPosition.BOTTOM,
+                      backgroundColor: Colors.green,
+                      colorText: Colors.white,
+                      margin: EdgeInsets.only(bottom: 10),
+                    );
+                  }
+                },
+                child: Text('Submit')),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  homeController.updateEmail('${storage.read('email')}');
+                  storage.listenKey('email', (_) {
+                    print('Value Change');
+                  });
+                },
+                child: Text('View')),
+            Obx(() => Text(homeController.email.value)),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
+import 'bindings/home_binding.dart';
 import 'home_page.dart';
 
-void main() {
+void main() async {
+  //await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -12,20 +16,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      getPages: [
+        GetPage(name: '/home', page: () => HomePage(), binding: HomeBinding()),
+      ],
       home: HomePage(),
     );
   }
